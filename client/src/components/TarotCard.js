2b580a6d81ac674d92240792b9bf7ba1994bd736
nathansabin <sabinnat001@gmail.com>
2023-08-25 19:55:24 -0700

    changed home page format. Setup login page and auth util.

--- client/src/components/TarotCard.js
+++ client/src/components/TarotCard.js
@@ -1,20 +1,15 @@
 import React, { useEffect } from 'react';
 
-const Cards = (props) => {
+const TarotCard = (props) => {
     return (
     <div>
         <div>
             <h3>{ props.tarot }</h3>
             <img src={ props.card } />
         </div>
-        <div>
-            <h3>{ props.horoscope }</h3>
-            <p>{ props.text }</p>
-        </div>
-        <div></div>
         {/* remove button*/}
     </div>
     );
 }
 
-export default Cards;
+export default TarotCard;
---
+++ client/src/components/horoscopeCard.js
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react';
+
+const HoroscopeCard = (props) => {
+    return (
+    <div>
+        <div>
+            <h3>{ props.horoscope }</h3>
+            <p>{ props.text }</p>
+        </div>
+        {/* remove button*/}
+    </div>
+    );
+}
+
+export default HoroscopeCard;
