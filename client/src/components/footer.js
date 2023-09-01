ec5dd8bd205ed1cf24d168b0b6d88dac2d9647d7
Baba II <127648521+allister-seras@users.noreply.github.com>
2023-08-31 17:02:34 -0700

    added footer

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -1,13 +1,23 @@
-import react, { useEfect, useEffect } from 'react'
+import React, { useEffect } from 'react';
+import '../App.css';
 
-const Footer = (props) => {
+const Footer = (props) => { 
   return (
-    <div>
-        <div>
-            <h3>footer :|</h3>
+    <footer>
+      <div className="footer-content">
+        <p>AstroLog is brought to you by The Capybara Cosmonauts</p>
+        <div className="logo">
+          <img src="assets/images/CC_Logo.png" alt="Capybara Cosmonauts Logo" width="120px" />
         </div>
-    </div>
-  )
-}
+        <div className="team-members">
+          <p>Allister Seras Kalag, Amanda Monroy, Leonela Ludeña, Nathan Sabin, & Michael O'Sullivan</p>
+        </div>
+        <div className="github-link">
+          <a href="https://github.com/allister-seras/AstroLog">GitHub Repository</a>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
