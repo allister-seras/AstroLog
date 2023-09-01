44746fd851d527b24c1b600cf393059298ca9552
Michael O'Sullivan <michaelhallosullivan@gmail.com>
2023-08-31 20:27:57 -0700

    added saved tarot and horoscope readings to journal model

--- client/src/components/journalForm.js
+++ client/src/components/journalForm.js
@@ -2,10 +2,13 @@ import React, { useState, useEffect } from 'react';
 
 const JournalForm = (props) => {
     return (
-    <div>
+    <div className='border border-dark content p-4'>
         <h3>{props.date}</h3>
+        <h3>Horoscope</h3>
         <p>{props.horoscope}</p>
+        <h3>Tarot</h3>
         <p>{props.tarot}</p>
+        <h3>Entry</h3>
         <p>{props.journalText}</p>
     </div>
     );
